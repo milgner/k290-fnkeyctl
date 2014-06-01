24634c2982fc362a0cb8664aa74a0ae7672ca5d1
Mariusz Smykuła <mariuszs@gmail.com>
2014-06-01 11:38:54 +0200

    Fix build script for Fedora20

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clang -std=c++11 -I/usr/include -L/usr/lib -lusb-1.0 -lstdc++ k290_fnkeyctl.cpp -o k290_fnkeyctl
+clang -std=c++11 -I/usr/include -L/usr/lib -lusb-1.0 k290_fnkeyctl.cpp -o k290_fnkeyctl -lstdc++ 
