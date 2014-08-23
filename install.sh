5d17d4aad27207f205c674cdd606e9d459d33b1c
Marcus Ilgner <marcus.ilgner@evopark.de>
2014-08-23 21:34:25 +0200

    Add conditional for systemd wakeup script

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ install -m 755 -o root k290_fnkeyctl /usr/local/sbin/
 if [ -d /etc/udev/rules.d ]; then
   install -m 644 -o root 99-k290-config.rules /etc/udev/rules.d/
 fi
-install -m 755 -o root k290-fnkeyctl.sh /usr/lib/systemd/system-sleep/
+if [ -d /usr/lib/systemd/system-sleep/ ]; then
+  install -m 755 -o root k290-fnkeyctl.sh /usr/lib/systemd/system-sleep/
+fi
 if [ -d /etc/pm/sleep.d ]; then
   install -m 755 -o root 20-k290.sh /etc/pm/sleep.d/
 fi
