2ed16d36433409ba6a99396ad486766bdc397e73
Alex Richardson <arichardson.kde@gmail.com>
2015-05-12 14:41:16 +0100

    Add CMake build system
    This ensures that the paths in the scripts/udev rule always correspond
    to where the binary was actually installed

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-# comment in or out to whatever compiler you prefer
-# if you want to use GCC, make sure it's a recent one
-g++ -std=gnu++0x k290_fnkeyctl.cpp `pkg-config --libs libusb-1.0` -o k290_fnkeyctl
-# clang -std=c++11 -I/usr/include -L/usr/lib k290_fnkeyctl.cpp -lusb-1.0 -lstdc++ -o k290_fnkeyctl
+cmake . $@
+make
